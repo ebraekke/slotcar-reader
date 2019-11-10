108ba02d6c7a1227a6caa843ef01eca9621fa021
ebraekke <ebraekke@yahoo.com>
2019-11-10 13:42:59 +0100

    added tests for different barcodes

---
+++ barcode_verify.py
@@ -0,0 +1,37 @@
+
+import pyzbar.pyzbar as pyzbar
+from PIL import Image
+
+
+
+# obj.data.decode('UTF-8')  => string of data
+def decode(im):
+    # Find barcodes and QR codes
+    decoded_objects = pyzbar.decode(im)
+
+    # Print results
+    for obj in decoded_objects:
+        print('Type : ', obj.type)
+        print('Data : ', obj.data, '\n')
+
+    return decoded_objects
+
+
+
+
+
+if __name __ == "main__":
+    img = Image.open('tests/resources/qr_frame.png')
+    img = Image.open('tests/resources/Capture_barcode.PNG')
+    img = Image.open('tests/resources/barcode_cloud_day.png')
+    pyzbar.decode(img)
+
+img = Image.open('tests/resources/qr_frame.png')
+img = Image.open('tests/resources/Capture_barcode.PNG')
+img = Image.open('tests/resources/barcode_cloud_day.png')
+
+img = Image.open('tests/resources/barcode_cloud_day.png')
+
+barcodes = pyzbar.decode(img)
+
+barcodes[0].data
